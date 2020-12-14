afe3a7dfd2af13ede1c125b5aaf8b0ac41916dac
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-12-14 20:04:35 +0530

    Creating business user details schema, controllers and routers file for shifting to business users

---
+++ models/BusinessUserDetails.js
@@ -0,0 +1,58 @@
+// Importing the depandencies...
+// Making a simple user Schema in mongoDB...
+
+const mongoose = require("mongoose")
+
+const businessUserDetailsSchema = new mongoose.Schema({
+    mainUserId: {
+        type: String,
+        required: true
+    },
+    mainUserRegistrationId: {
+        type: String,
+        required: true
+    },
+    businessName: {
+        type: String,
+        required: true
+    },
+    businessOwnerName: {
+        type: String,
+        required: true,
+    },
+    businessOwnerPhoneNo: {
+        type: String,
+        required: true
+    },
+    businessOwnerPanNo: {
+        type: String,
+        required: true
+    },
+    businessOwnerPanCard: {
+        type: String,
+        required: true
+    },
+    businessVarified: {
+        type: Boolean,
+        default: false
+    },
+    ownerAddressPincode: {
+        type: String,
+        required: true,
+    },
+    ownerAddressState: {
+        type: String,
+        required: true,
+    },
+    businessType: {
+        type: String,
+        required: true
+    },
+    shippingPincodes: [String],
+    paymentUPIId: {
+        type: String,
+        required: true
+    },    
+})
+
+module.exports = mongoose.model("businessuserdetail", businessUserDetailsSchema)
