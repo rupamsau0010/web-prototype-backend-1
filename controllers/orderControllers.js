84da7049f8f9a60f3eccc3270135681795616fb0
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-12-26 13:01:21 +0530

    making razorpay route for paying(without storeing the data and without webhooks implementation)

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,8 +1,10 @@
 // Import depandencies
+const { v4: uuidv4 } = require("uuid");
 
 // Import local depandencies
 const User = require("../models/Users");
-const razorpay = require("../services/razorpaysdk")
+const Product = require("../models/Products");
+const razorpay = require("../services/razorpaysdk");
 
 module.exports.preOrderAddress_post = async (req, res) => {
   // Import data from req.params
@@ -39,3 +41,47 @@ module.exports.preOrderAddress_post = async (req, res) => {
   );
 };
 
+module.exports.razorpay_post = async (req, res) => {
+  // Get data from req.params
+
+  // Get data from req.body
+  const userId = req.body.userId;
+
+  // Find the user and get the cart details
+  const user = await User.findById({ _id: userId });
+
+  // Total value to be paid
+  var i = 0,
+  var total = 0;
+  for (i = 0; i < user.cart.length; i++) {
+    // Find product and get the price
+    const product = await Product.findById({ _id: user.cart[i] });
+    total += product.price;
+
+    if (i == user.cart.length - 1) {
+      const payment_capture = 1;
+      const amount = total;
+      const currency = "INR";
+
+      const options = {
+        amount: amount * 100,
+        currency,
+        receipt: uuidv4(),
+        payment_capture,
+      };
+
+      try {
+        const response = await razorpay.orders.create(options);
+        console.log(response);
+
+        res.json({
+          id: response.id,
+          currency: response.currency,
+          amount: response.amount,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,5 +8,8 @@ const orderControllers = require("../controllers/orderControllers")
 // Update the user data(phoneNo, deliveryLandmark, deliveryPincode) before order
 router.post("/updateaddress", orderControllers.preOrderAddress_post)
 
+// Get payment from the user
+router.post("/razorpay", orderControllers.razorpay_post)
+
 // Export the module
 module.exports = router
