ae483941354f59d20773b2e81c9388b3c4581320
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-12-26 16:18:42 +0530

    changing the razorpay controllers for paying for a simgle product

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -46,42 +46,45 @@ module.exports.razorpay_post = async (req, res) => {
 
   // Get data from req.body
   const userId = req.body.userId;
+  const productId = req.body.productId;
 
-  // Find the user and get the cart details
-  const user = await User.findById({ _id: userId });
+  // Get the price of the product
+  const product = await Product.findById({ _id: productId });
+  const price = product.price;
 
-  // Total value to be paid
-  var i = 0,
-  var total = 0;
-  for (i = 0; i < user.cart.length; i++) {
-    // Find product and get the price
-    const product = await Product.findById({ _id: user.cart[i] });
-    total += product.price;
-
-    if (i == user.cart.length - 1) {
-      const payment_capture = 1;
-      const amount = total;
-      const currency = "INR";
-
-      const options = {
-        amount: amount * 100,
-        currency,
-        receipt: uuidv4(),
-        payment_capture,
-      };
-
-      try {
-        const response = await razorpay.orders.create(options);
-        console.log(response);
-
-        res.json({
-          id: response.id,
-          currency: response.currency,
-          amount: response.amount,
-        });
-      } catch (error) {
-        console.log(error);
+  // Remove the item from the cart(if it was)
+  User.findByIdAndUpdate(
+    { _id: userId },
+    { $pull: { cart: productId } },
+    { new: true },
+    function (err1, data1) {
+      if (data1 && !err1) {
+        const payment_capture = 1;
+        const amount = price;
+        const currency = "INR";
+  
+        const options = {
+          amount: amount * 100,
+          currency,
+          receipt: uuidv4(),
+          payment_capture,
+        };
+  
+        try {
+          const response = await razorpay.orders.create(options);
+          console.log(response);
+  
+          res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount,
+          });
+        } catch (error) {
+          console.log(error);
+        }
       }
     }
-  }
+  );
 };
+
+
